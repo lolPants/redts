004447f99d61823290b3c206770b091d8897eb93
Jack Baron <jackmbaron@gmail.com>
2021-04-15 10:11:54 +0100

    sync every day

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import 'source-map-support/register.js'
 
 import { field } from '@lolpants/jogger'
+import { scheduleJob } from 'node-schedule'
 import { PORT } from './env/index.js'
 import { logger } from './logger.js'
 import { app } from './server.js'
@@ -8,6 +9,10 @@ import { syncDB } from './update.js'
 
 const boot = async () => {
   await syncDB(true)
+
+  scheduleJob('30 5 * * *', () => {
+    void syncDB(false)
+  })
 
   app.listen(PORT).on('listening', () => {
     logger.info(field('status', 'ready'), field('port', PORT))
