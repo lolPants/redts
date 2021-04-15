c77808a75fdbfe7133416e11b938cb2ab87301b6
Jack Baron <jackmbaron@gmail.com>
2021-04-15 18:11:53 +0100

    log duration

--- src/logger.ts
+++ src/logger.ts
@@ -34,11 +34,14 @@ const methodField = createField('method')
 const urlField = createField('url')
 const statusField = createField('status')
 const sizeField = createField('size')
+const durationField = createField('duration')
 const uaField = createField('userAgent')
 const referrerField = createField('referrer')
 
 export const middleware: Middleware = async (ctx, next) => {
+  const before = Date.now()
   await next()
+  const duration = (Date.now() - before) / 1000
 
   if (IS_DEV === true || ENABLE_LOGGING === true) {
     const fields = [
@@ -49,6 +52,7 @@ export const middleware: Middleware = async (ctx, next) => {
       urlField(ctx.url),
       statusField(ctx.status),
       sizeField(ctx.status === 204 ? 0 : ctx.response.length ?? -1),
+      durationField(duration),
       uaField(ctx.headers['user-agent'] ?? '-'),
       referrerField(ctx.headers.referer ?? ctx.headers.referrer ?? '-'),
     ] as const
