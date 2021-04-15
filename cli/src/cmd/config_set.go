4c7f3373a570e7de37a7916ed0ed46ba9525ee81
Jack Baron <jackmbaron@gmail.com>
2021-04-16 00:32:03 +0100

    add config set command

---
+++ cli/src/cmd/config_set.go
@@ -0,0 +1,56 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/lolPants/redts/cli/src/pkg/config"
+	"github.com/spf13/cobra"
+)
+
+var (
+	configSetCmd = &cobra.Command{
+		Use:   "set",
+		Short: "set config key value",
+		Args:  cobra.ExactArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			key := strings.ToLower(args[0])
+			value := args[1]
+
+			modified := false
+			cfg := config.Load()
+
+			if key == "url" {
+				cfg.URL = value
+				modified = true
+			}
+
+			if key == "username" {
+				cfg.Username = value
+				modified = true
+			}
+
+			if key == "token" {
+				cfg.Token = value
+				modified = true
+			}
+
+			if modified {
+				err := cfg.Save()
+				if err != nil {
+					panic(err)
+				}
+
+				return
+			}
+
+			fmt.Fprintf(os.Stderr, "error: invalid config key `%s`\n", key)
+			os.Exit(1)
+		},
+	}
+)
+
+func init() {
+	configCmd.AddCommand(configSetCmd)
+}
