030d3d5bd438ac5c423ecab95d9e88c1c392d2f1
Jack Baron <jackmbaron@gmail.com>
2021-04-15 09:13:46 +0100

    log on ready

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import 'source-map-support/register.js'
 
+import { field } from '@lolpants/jogger'
 import { PORT } from './env/index.js'
+import { logger } from './logger.js'
 import { app } from './server.js'
 
-app.listen(PORT)
+app.listen(PORT).on('listening', () => {
+  logger.info(field('status', 'ready'), field('port', PORT))
+})
